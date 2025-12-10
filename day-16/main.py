62dd3921bf8ff17a776ec9683639b4d301f1a202
Filipe Augusto <filipecordeiro213@gmail.com>
2025-12-10 19:30:13 -0300

    How to create your own Class

--- day-16/main.py
+++ day-16/main.py
@@ -1,16 +1,28 @@
 # import another_module
 # print(another_module.another_variable)
 
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.color("DarkSlateBlue")
-timmy.shape("turtle")
-timmy.forward(100)
-timmy.left(50)
-timmy.forward(100)
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.color("DarkSlateBlue")
+# timmy.shape("turtle")
+# timmy.forward(100)
+# timmy.left(50)
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
+# my_screen = Screen()
+# print(my_screen.canvheight)
 
-my_screen.exitonclick()
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()  
+table.field_names = ["Pokemon Name", "Types"]
+table.add_row(["Pikachu", "Eletric"])
+table.add_row(["Squirtle", "Aquatic"])
+table.add_row(["Charmander", "Fire"])
+
+table.align = "l"
+
+print(table)
