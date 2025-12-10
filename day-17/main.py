e385bd905ad790a59316ea72bfb2700092eb2444
Filipe Augusto <filipecordeiro213@gmail.com>
2025-12-10 19:48:33 -0300

    Working with attributes, class constructors and __init__ func

--- day-17/main.py
+++ day-17/main.py
@@ -1,4 +1,16 @@
 class User:
-    pass
+    # this function is call ever time a new object is created
+        # self is the actual object that's being created or being initialized
+    def __init__(self, user_id, user_name):
+        # print("new user being created...")
+        self.id = user_id
+        self.name = user_name
 
-user_1 = User()
+
+user_1 = User("001", "Filipe")
+
+print(user_1.name)
+
+# user_2 = User()
+# user_2.id = "002"
+# user_2.name = "angela"
