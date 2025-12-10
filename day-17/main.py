34fabc6bbd0cac58eca2d1c1ea15e5c033347d86
Filipe Augusto <filipecordeiro213@gmail.com>
2025-12-10 20:52:31 -0300

    Adding Methods to a Class

--- day-17/main.py
+++ day-17/main.py
@@ -5,12 +5,20 @@ class User:
         # print("new user being created...")
         self.id = user_id
         self.name = user_name
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("001", "Filipe")
+user_2 = User("002", "Ana")
 
-print(user_1.name)
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
 
-# user_2 = User()
-# user_2.id = "002"
-# user_2.name = "angela"
+print(user_2.followers)
+print(user_2.following)
