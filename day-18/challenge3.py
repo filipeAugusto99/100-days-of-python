8940fa99c03d70fedfd7d8b53464beeba786fa3d
Filipe Augusto <filipecordeiro213@gmail.com>
2025-12-19 16:18:32 -0300

    improv challenge3

--- day-18/challenge3.py
+++ day-18/challenge3.py
@@ -1,72 +1,30 @@
 from turtle import Turtle, Screen
-
-
-def triangle():
-    t.color("CadetBlue")
-    for _ in range(3):
-        t.forward(50)
-        t.right(120)
-
-
-def square():
-    t.color("brown")
-    for _ in range(4):
-        t.forward(50)
-        t.right(90)
-
-
-def pentagon():
-    t.color("chocolate")
-    for _ in range(5):
-        t.forward(50)
-        t.right(72)
-
-
-def hexagon():
-    t.color("navy")
-    for _ in range(6):
-        t.forward(50)
-        t.right(60)
-
-
-def heptagon():
-    t.color("OrangeRed")
-    for _ in range(7):
-        t.forward(50)
-        t.right(51.42857142857143)
-
-
-def octagon():
-    t.color("RoyalBlue")
-    for _ in range(8):
-        t.forward(50)
-        t.right(45)
-
-
-def nonagon():
-    t.color("green")
-    for _ in range(9):
-        t.forward(50)
-        t.right(40)
-
-
-def decagon():
-    t.color("blue")
-    for _ in range(10):
-        t.forward(50)
-        t.right(36)
-
+import random
 
 t = Turtle()
 
-triangle()
-square()
-pentagon()
-hexagon()
-heptagon()
-octagon()
-nonagon()
-decagon()
+colours = [
+    "gainsboro",
+    "cornflower blue",
+    "orange red",
+    "red",
+    "blue",
+    "black",
+    "yellow",
+    "dark slate blue",
+]
+
+
+def draw_shape(num_slides):
+    angle = 360 / num_slides
+    for _ in range(num_slides):
+        t.forward(100)
+        t.right(angle)
+
+
+for shape_side_n in range(3, 11):
+    t.color(random.choice(colours))
+    draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()
