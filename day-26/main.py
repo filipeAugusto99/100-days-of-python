01300b1fade7d74d168c2838364c890e7c4cd79d
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-06 14:26:17 -0300

    day 26

---
+++ day-26/main.py
@@ -0,0 +1,27 @@
+import pandas
+
+# List Comprehension
+# new_list = [new_item for item in list if condiction]
+
+# Dict Comprehension
+    # For lists:
+        # new_dict = {new_key:new_value for item in list if condiction}
+    # For dicts:
+        # new_dict = {new_key:new_value for (key,value) in dict.items() if condiction}
+
+
+student_scores = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+student_data_frame = pandas.DataFrame(student_scores)
+
+# Loop through a data frame
+# for (key, value) in  student_data_frame.items():
+#     print(value)
+
+
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    print(row.score)
