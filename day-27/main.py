1876ed4214cf287a5bbb57e88df62ba87c277279
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-07 15:01:18 -0300

    day-27

--- day-27/main.py
+++ day-27/main.py
@@ -1,14 +1,39 @@
-import tkinter
+from tkinter import *
 
 # Create a window
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # Label
 
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.grid(column=0, row=0)
+
+# Change prop of the labels
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+my_label.config(padx=50, pady=50)
+
+# Button
+def button_clicked():
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click me", command=button_clicked)
+button.grid(column=1, row=1)
+
+# New Button
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+# Entry
+
+input = Entry(width=10, validate="focus")
+input.grid(column=3, row=2)
+
 
 
 
