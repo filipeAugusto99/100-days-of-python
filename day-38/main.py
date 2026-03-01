7dc0d15d4ebca7cd2f0242b06918fdd5f1f1a2c8
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-03-01 14:20:20 -0300

    step 02

--- day-38/main.py
+++ day-38/main.py
@@ -1,4 +1,23 @@
-import os
+import requests
 
-APP_ID = os.environ.get("API_ID")
-API_KEY = os.environ.get("API_KEY")
+
+APP_ID = "app_c5675b8137bb46a2a331a26a"
+API_KEY = "nix_live_laVhbwIv4ZJ6VAy53Lr2t5L2lAHg9vwG"
+
+
+main_url_endpoint = "https://app.100daysofpython.dev/v1/nutrition/natural/exercise"
+
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
+
+
+exercise_params = {
+    "query": input("Tell me which exercises you did: ")
+}
+
+
+response = requests.post(url=main_url_endpoint, json=exercise_params, headers=headers)
+print(response.text)
