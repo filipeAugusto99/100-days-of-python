16f56ac91493c6eb84c4d93dc64ddc5c4c2a85cb
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-03-02 15:42:56 -0300

    Step 04

--- day-38/main.py
+++ day-38/main.py
@@ -1,5 +1,5 @@
 import requests
-
+from datetime import datetime
 
 APP_ID = "app_c5675b8137bb46a2a331a26a"
 API_KEY = "nix_live_laVhbwIv4ZJ6VAy53Lr2t5L2lAHg9vwG"
@@ -21,3 +21,28 @@ exercise_params = {
 
 response = requests.post(url=main_url_endpoint, json=exercise_params, headers=headers)
 print(response.text)
+
+
+json_response = response.json()
+now = datetime.now()
+formatted_date = now.strftime("%d/%m/%Y")
+formatted_time = now.strftime("%H:%M:%S")
+
+
+google_sheet_endpoint = "https://api.sheety.co/e21a8e91526b16245a7b34dab6850543/myWorkouts/workouts"
+
+
+for exercise in json_response["exercises"]:
+    workout_sheets = {
+        "workout": {
+            "date": formatted_date,
+            "time": formatted_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+    sheet_response = requests.post(url=google_sheet_endpoint, json=workout_sheets)
+    print(sheet_response.status_code)
+    print(sheet_response.text)
