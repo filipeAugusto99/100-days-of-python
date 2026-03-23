1970640e47ca979cbf9fd254140d4e9405b5f0b6
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-03-23 17:31:37 -0300

    Finding and Selecting Particular Elements with BeautifulSoup

--- day-45/bs4-start/main.py
+++ day-45/bs4-start/main.py
@@ -8,3 +8,26 @@ soup = BeautifulSoup(contents, "html.parser")
 # print(soup.title.string)
 # print(soup)
 # print(soup.p)
+
+
+# Finding and Selecting Particular Elements with Beautiful Soup
+# all_anchor_tags = soup.find_all(name="a")
+#
+# for tag in all_anchor_tags:
+#     # print(tag.getText)
+#     print(tag.get("href"))
+#
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+#
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.get("class"))
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
+
+name = soup.select_one(selector="#name")
+print(name)
+
+headings = soup.select(".heading")
+print(headings)
