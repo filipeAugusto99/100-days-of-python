b4a1acba423ce1655928d2251020d42c7eeb8ac1
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-03-24 21:07:14 -0300

    Scraping a Live Website

--- day-45/bs4-start/main.py
+++ day-45/bs4-start/main.py
@@ -1,33 +1,66 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html") as file:
-    contents = file.read()
+response = requests.get("https://news.ycombinator.com/news")
 
-soup = BeautifulSoup(contents, "html.parser")
-# print(soup.title)
-# print(soup.title.string)
-# print(soup)
-# print(soup.p)
+yc_web_pag = response.text
+
+soup = BeautifulSoup(yc_web_pag, "html.parser")
+articles = soup.find_all(name="span", class_="titleline")
+
+article_texts = []
+article_links = []
+
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.find(name="a").get("href")
+    article_links.append(link)
+
+article_upvote = [score.getText() for score in soup.find_all(name="span", class_="score")]
 
 
-# Finding and Selecting Particular Elements with Beautiful Soup
-# all_anchor_tags = soup.find_all(name="a")
-#
-# for tag in all_anchor_tags:
-#     # print(tag.getText)
-#     print(tag.get("href"))
-#
-# heading = soup.find(name="h1", id="name")
-# print(heading)
-#
-# section_heading = soup.find(name="h3", class_="heading")
-# print(section_heading.get("class"))
+print(article_texts)
+print(article_links)
+print(article_upvote)
 
-company_url = soup.select_one(selector="p a")
-print(company_url)
 
-name = soup.select_one(selector="#name")
-print(name)
 
-headings = soup.select(".heading")
-print(headings)
+
+
+
+
+
+
+
+# with open("website.html") as file:
+#     contents = file.read()
+# 
+# soup = BeautifulSoup(contents, "html.parser")
+# # print(soup.title)
+# # print(soup.title.string)
+# # print(soup)
+# # print(soup.p)
+# 
+# 
+# # Finding and Selecting Particular Elements with Beautiful Soup
+# # all_anchor_tags = soup.find_all(name="a")
+# #
+# # for tag in all_anchor_tags:
+# #     # print(tag.getText)
+# #     print(tag.get("href"))
+# #
+# # heading = soup.find(name="h1", id="name")
+# # print(heading)
+# #
+# # section_heading = soup.find(name="h3", class_="heading")
+# # print(section_heading.get("class"))
+# 
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
+# 
+# name = soup.select_one(selector="#name")
+# print(name)
+# 
+# headings = soup.select(".heading")
+# print(headings)
